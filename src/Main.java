8d372ac394e398e3953908d651edaf9927d6a6ad
0rthodox <long.popov@gmail.com>
2019-10-08 01:00:05 +0300

    cleaned main

--- src/Main.java
+++ src/Main.java
@@ -3,20 +3,5 @@ import ru.mipt.collections.NikiList;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Should work for now..");
-        NikiList testList = new NikiList(3);
-        testList.add(1);
-        testList.add(2);
-        testList.add(3);
-        testList.add(4);
-        testList.add(4);
-        testList.add(4);
-        NikiList sublist = new NikiList(0);
-        sublist.add(3);
-        sublist.add(4);
-        System.out.println(testList.size());
-        System.out.println(testList.remove(4));
-        System.out.println(testList.size());
-
-        System.out.println(testList.get(0));
     }
 }
